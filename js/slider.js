49a154c4aed4cf6fbac5ff3b9cef60d921123d36
KMJ_DEV <dds13462@gmail.com>
2023-01-03 18:52:55 +0900

    modify

--- js/slider.js
+++ js/slider.js
@@ -1,25 +1,42 @@
-$(function(){
-
 const visual = $("#brandVisual>ul>li");
 const button = $("#buttonList>li");
-let num=$(".slider_wrap #slider_num")
+let play = $(".controls li:nth-child(2)");
+let num = $(".slider_wrap #slider_num");
 let current = 0;
 let id;
+let check = false;
+let btnIdx = 0;
+const speed = 1000;
 
-/* for(i=0;i<visual.length;i++){
-  console.log(i);
-} */
+let textArr=["온천","유성","축제"]
 
-let btnIdx = 0; 
-const speed = 6000;
+num.text(visual.length)
 
-//버튼클릭함수
-button.click(function () {
-  btnIdx = $(this).index();
 
-  button.removeClass("on");
-  $(this).addClass("on");
-  move();
+// play.click(() => {
+//   if (play.text() == "stop") {
+//     play.text("play");
+//     play.removeClass("stop");
+//     play.addClass("play"), clearInterval(id);
+
+//   } else {
+//     play.text("stop");
+//     play.removeClass("play");
+//     play.addClass("stop");
+//     timer();
+//   }
+// });
+
+play.click(() => {
+  if (play.attr("class") === "stop") {
+    play.removeClass("stop");
+    play.addClass("play");
+    clearInterval(id);
+  } else {
+    play.removeClass("play");
+    play.addClass("stop");
+    timer();
+  }
 });
 
 //시간마다 실행할 함수
@@ -27,12 +44,24 @@ timer();
 function timer() {
   id = setInterval(function () {
     let next = current + 1;
+    /* $("#slider_num2").text(current+1) */
     if (next == visual.length) {
       next = 0;
     }
     button.eq(next).trigger("click");
   }, speed);
 }
+
+//버튼클릭함수
+button.click(function () {
+  btnIdx = $(this).index();
+
+  $("#slider_num2").text(current+1)
+
+  button.removeClass("on");
+  $(this).addClass("on");
+  move();
+});
 
 //슬라이드 이동시키는 함수
 function move() {
@@ -48,80 +77,48 @@ function move() {
 }
 
 /* 자동재생 멈춤 */
-clearAuto();
+ clearAuto();
 function clearAuto() {
-  $("#brandVisual,#buttonList, .controls").mouseenter(function () {
+  $("#brandVisual,#buttonList, .prev,.next").mouseenter(function () {
     clearInterval(id);
   });
-  $("#brandVisual ,#buttonList, .controls").mouseleave(function () {
+  $("#brandVisual ,#buttonList, .prev,.next").mouseleave(function () {
     timer();
   });
 }
 
 /* 좌우 컨트롤 버튼 */
 controls();
-function controls(){
-  $('.controls .next').click(function(){
-    btnIdx =btnIdx+1;
+function controls() {
+  $(".controls .next").click(function () {
+    btnIdx = btnIdx + 1;
 
-    if(btnIdx == visual.length){
-      btnIdx=0;
+    if (btnIdx == visual.length) {
+      btnIdx = 0;
     }
     button.removeClass("on");
-    button.eq(btnIdx).addClass('on');
+    button.eq(btnIdx).addClass("on");
 
     let cu = visual.eq(current);
     let ne = visual.eq(btnIdx);
-    cu.css("left","0").stop().animate({left:"-100%"})
-    ne.css("left","100%").stop().animate({left:"0%"})
-    current=btnIdx;
-  })
+    cu.css("left", "0").stop().animate({ left: "-100%" });
+    ne.css("left", "100%").stop().animate({ left: "0%" });
+    current = btnIdx;
+  });
 
-  $('.controls .prev').click(function(){
-    btnIdx =btnIdx-1;
+  $(".controls .prev").click(function () {
+    btnIdx = btnIdx - 1;
 
-    if(current == 0){
-      btnIdx=visual.length-1;
+    if (current == 0) {
+      btnIdx = visual.length - 1;
     }
     button.removeClass("on");
-    button.eq(btnIdx).addClass('on');
+    button.eq(btnIdx).addClass("on");
 
     let cu = visual.eq(current);
     let pr = visual.eq(btnIdx);
-    cu.css("left","0").stop().animate({left:"100%"})
-    pr.css("left","-100%").stop().animate({left:"0%"})
-    current=btnIdx;
-  })
-
-}
-
-
-var tabWrapper = $(".tab-wrapper");
-
-tabWrapper.each(function () {
-  var currentElement = $(this);
-  var menus = currentElement.find(".pick_btn a");
-  var panels = currentElement.find(".tab-content > ul");
-
-  menus.eq(0).addClass("active");
-  panels.eq(0).show();
-
-  menus.click(function (e) {
-    e.preventDefault();
-
-    let tg = $(this);
-    let currentLink = tg.attr("href");
-    console.log(currentLink);
-
-    panels.hide();
-    $(currentLink).show();
-
-    menus.removeClass("active");
-    tg.addClass("active");
+    cu.css("left", "0").stop().animate({ left: "100%" });
+    pr.css("left", "-100%").stop().animate({ left: "0%" });
+    current = btnIdx;
   });
-});
-
-
-
-
-})
+}
---
+++ js/tab.js
@@ -0,0 +1,19 @@
+var menus = $(".pick_btn a");
+var panels = $(".tab-content > ul");
+
+menus.eq(0).addClass("active");
+panels.eq(0).show().css("display","flex");
+
+console.log(panels);
+menus.click(function (e) {
+  e.preventDefault();
+
+  let tg = $(this);
+  let currentLink = tg.attr("href");
+
+  panels.hide();
+  $(currentLink).show().css("display","flex");
+
+  menus.removeClass("active");
+  tg.addClass("active");
+});
