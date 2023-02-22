2971e8645da1f7f0047dda5fbb263c3feeffa494
KMJ_DEV <dds13462@gmail.com>
2023-02-22 15:02:50 +0900

    notice슬라이더 수정

--- js/notice.js
+++ js/notice.js
@@ -2,14 +2,19 @@ const notice = $("#noticeSlider>li");
 const next1 = $(".next a");
 let stop = $(".stop");
 let current1 = 0;
-let id1;
+let stopMoving;
 let btnIdx1 = 0;
-const speed1 = 1000;
-console.log("notice", notice);
+const speed1 = 2000;
+let noticeWrap,newNotice;
 
-notice.each(function (i, o) {
-  $(o).css({ top: 100 * i + "%" });
-});
+let cloneNotice=notice.clone().addClass("clone");
+noticeWrap=$("#noticeSlider").append(cloneNotice);
+newNotice=$("#noticeSlider>li")
+
+newNotice.each(function (i, o) {
+	$(o).css({ top: 100 * i + "%" });
+}); 
+
 /*
 stop.click(() => {
   if (stop.attr("class") === "stop") {
@@ -25,76 +30,71 @@ stop.click(() => {
 */
 
 //시간마다 실행할 함수
-timer();
-function timer() {
-  id1 = setInterval(function () {
-    console.log("dd")
-    let next = current1 + 1;  //0+1 (1)
-    if (next == notice.length) {
-      next = 0;
-    }
-   // next1.trigger("click");
-  }, speed1);
+autoMoving();
+function autoMoving() {
+	stopMoving=setInterval(() => {
+		move(btnIdx1++);
+	}, speed1);
 }
-
-
 
 //슬라이드 이동시키는 함수
 function move() {
-  if (current1 == btnIdx1) {
-    return;
+  if(btnIdx1>=notice.length){
+    btnIdx1=0;
   }
-  let cu = notice.eq(current1);
-  let ne = notice.eq(btnIdx1);
-  cu.css("top", "0").stop().animate({ top: "-100%" });
-  ne.css("top", "100%").stop().animate({ top: "0%" });
+	if (current1 == btnIdx1) {
+		return;
+	}
+	let cu = newNotice.eq(current1);
+	let ne = newNotice.eq(btnIdx1);
+	cu.css("top", "0").stop().animate({ top: "-100%" });
+	ne.css("top", "100%").stop().animate({ top: "0%" });
 
-  current1 = btnIdx1;
+	current1 = btnIdx1;
 }
 
 /* 자동재생 멈춤 */
-//clearAuto();
+clearAuto();
 function clearAuto() {
-  $("#noticeSlider,.next1, .prev,.next").mouseenter(function () {
-    clearInterval(id1);
-  });
-  $("#noticeSlider ,.next1, .prev,.next").mouseleave(function () {
-    timer();
-  });
+	$("#noticeSlider,.next1, .prev,.next").mouseenter(function () {
+		clearInterval(stopMoving);
+	});
+	$("#noticeSlider ,.next1, .prev,.next").mouseleave(function () {
+		autoMoving();
+	});
 }
 
 /* 좌우 컨트롤 버튼 */
 controls();
 function controls() {
-  $(".notice_controls .next").click(function () {
-    btnIdx1 = btnIdx1 + 1;
+	$(".notice_controls .next").click(function () {
+		btnIdx1 = btnIdx1 + 1;
+		if (btnIdx1 == notice.length) {
+			btnIdx1 = 0;
+		}
+		next1.removeClass("on");
+		next1.eq(btnIdx1).addClass("on");
 
-    if (btnIdx1 == notice.length) {
-      btnIdx1 = 0;
-    }
-    next1.removeClass("on");
-    next1.eq(btnIdx1).addClass("on");
+		let cu = notice.eq(current1);
+		let ne = notice.eq(btnIdx1);
+		cu.css("top", "0").stop().animate({ top: "-100%" });
+		ne.css("top", "100%").stop().animate({ top: "0%" });//
+		current1 = btnIdx1;
+	});
 
-    let cu = notice.eq(current1);
-    let ne = notice.eq(btnIdx1);
-    cu.css("top", "0").stop().animate({ top: "-100%" });
-    ne.css("top", "100%").stop().animate({ top: "0%" });
-    current1 = btnIdx1;
-  });
+	$(".notice_controls .prev").click(function () {
+		btnIdx1 = btnIdx1 - 1;
 
-  $(".notice_controls .prev").click(function () {
-    btnIdx1 = btnIdx1 - 1;
+		if (current1 == 0) {
+			btnIdx1 = notice.length - 1;
+		}
+		next1.removeClass("on");
+		next1.eq(btnIdx1).addClass("on");
 
-    if (current1 == 0) {
-      btnIdx1 = notice.length - 1;
-    }
-    next1.removeClass("on");
-    next1.eq(btnIdx1).addClass("on");
-
-    let cu = notice.eq(current1);
-    let pr = notice.eq(btnIdx1);
-    cu.css("top", "0").stop().animate({ top: "100%" });
-    pr.css("top", "-100%").stop().animate({ top: "0%" });
-    current1 = btnIdx1;
-  });
+		let cu = notice.eq(current1);
+		let pr = notice.eq(btnIdx1);
+		cu.css("top", "0").stop().animate({ top: "100%" });
+		pr.css("top", "-100%").stop().animate({ top: "0%" });
+		current1 = btnIdx1;
+	});
 }
